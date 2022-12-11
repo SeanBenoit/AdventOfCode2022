ba7c2d22feff37eb7aa50871abb1e69d2f445444
Sean Benoit <sean.joseph.benoit@gmail.com>
2022-12-11 02:22:36 -0500

    Day 11 optimized but trash

--- src/day11/Monkey.kt
+++ src/day11/Monkey.kt
@@ -1,31 +1,42 @@
 package day11
 
-import java.math.BigInteger
+import java.lang.Exception
 import java.util.*
+import kotlin.Long.Companion.MAX_VALUE
+import kotlin.math.sqrt
+
+// This is the least common multiple of the numbers each monkey is dividing by.
+// Calculating every number mod this doesn't change the checks but keeps things small.
+const val modFactor = 9_699_690L
 
 class Monkey(
-        startingItems: List<BigInteger>,
-        private val worryReductionFactor: BigInteger,
-        private val operation: (BigInteger) -> BigInteger,
-        private val test: (BigInteger) -> Boolean,
+        startingItems: List<Long>,
+        private val worryReductionFactor: Long,
+        private val operation: (Long) -> Long,
+        private val test: (Long) -> Boolean,
         private val targetTrue: Int,
         private val targetFalse: Int,
 ) {
-    private val items: Queue<BigInteger>
+    private val items: Queue<Long>
 
     init {
         items = LinkedList(startingItems)
     }
 
-    var itemsInspected = 0
+    var itemsInspected = 0L
 
-    fun processItems(): List<Pair<Int, BigInteger>> {
-        val itemsSent = mutableListOf<Pair<Int, BigInteger>>()
+    fun processItems(): List<Pair<Int, Long>> {
+        val itemsSent = mutableListOf<Pair<Int, Long>>()
         while (items.isNotEmpty()) {
             var item = items.remove()
             itemsInspected++
 
-            item = operation(item)
+            if (item > sqrt(MAX_VALUE.toDouble())) {
+                println("well, fuck $targetTrue $targetFalse")
+                throw Exception()
+            }
+
+            item = operation(item) % modFactor
 
             item /= worryReductionFactor
 
@@ -39,27 +50,27 @@ class Monkey(
         return itemsSent
     }
 
-    fun addItem(item: BigInteger) {
+    fun addItem(item: Long) {
         items.add(item)
     }
 
     companion object {
-        fun fromString(input: List<String>, worryReductionFactor: BigInteger): Monkey {
+        fun fromString(input: List<String>, worryReductionFactor: Long): Monkey {
             val startingItems = input[1].removePrefix("Starting items: ")
                     .split(", ")
-                    .map { it.toBigInteger() }
+                    .map { it.toLong() }
 
             val operationParts = input[2].removePrefix("Operation: new = old ")
                     .split(" ")
-            val operationValue = operationParts[1].toBigIntegerOrNull()
-            val operation: (BigInteger) -> BigInteger = when (operationParts[0]) {
-                "+" -> { value: BigInteger -> value + (operationValue ?: value) }
-                "*" -> { value: BigInteger -> value * (operationValue ?: value) }
+            val operationValue = operationParts[1].toLongOrNull()
+            val operation: (Long) -> Long = when (operationParts[0]) {
+                "+" -> { value: Long -> value + (operationValue ?: value) }
+                "*" -> { value: Long -> value * (operationValue ?: value) }
                 else -> throw UnsupportedOperationException("Unrecognized operation: ${operationParts[0]}")
             }
 
-            val testValue = input[3].removePrefix("Test: divisible by ").toBigInteger()
-            val test = { value: BigInteger -> value % testValue == 0.toBigInteger() }
+            val testValue = input[3].removePrefix("Test: divisible by ").toLong()
+            val test = { value: Long -> value % testValue == 0.toLong() }
 
             val targetTrue = input[4].removePrefix("If true: throw to monkey ").toInt()
 
@@ -77,7 +88,7 @@ class Monkey(
     }
 }
 
-fun engageInMonkeyBusiness(input: List<String>, worryReductionFactor: BigInteger, rounds: Int): Int {
+fun engageInMonkeyBusiness(input: List<String>, worryReductionFactor: Long, rounds: Int): Long {
     val monkeys = input.asSequence()
             .map { it.trim() }
             .chunked(7)
@@ -102,13 +113,13 @@ fun engageInMonkeyBusiness(input: List<String>, worryReductionFactor: BigInteger
 }
 
 fun solvePuzzle1(input: List<String>) {
-    val result = engageInMonkeyBusiness(input, 3.toBigInteger(), 20)
+    val result = engageInMonkeyBusiness(input, 3L, 20)
 
     println(result)
 }
 
 fun solvePuzzle2(input: List<String>) {
-    val result = engageInMonkeyBusiness(input, 1.toBigInteger(), 10_000)
+    val result = engageInMonkeyBusiness(input, 1L, 10_000)
 
     println(result)
 }
